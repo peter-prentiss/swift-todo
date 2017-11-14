85fa8eb5984ecd6f9cfb7dcadd030e9e8522b5b6
Peter <peter.prentiss@gmail.com>
2017-11-13 18:52:47 -0600

    Create basic, hard-coded todo list

---
+++ ToDoList/ToDo.swift
@@ -0,0 +1,14 @@
+//
+//  ToDo.swift
+//  ToDoList
+//
+//  Created by admin on 11/13/17.
+//  Copyright © 2017 admin. All rights reserved.
+//
+
+import UIKit
+
+class ToDo {
+    var name = ""
+    var important = false
+}
