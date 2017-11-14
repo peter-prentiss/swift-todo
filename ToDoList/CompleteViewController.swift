014ab31900554b4ebc56368014a8e17ef4df3cf4
Peter <peter.prentiss@gmail.com>
2017-11-13 20:33:40 -0600

    Adding and completing todos functioning

--- ToDoList/CompleteViewController.swift
+++ ToDoList/CompleteViewController.swift
@@ -21,13 +21,16 @@ class CompleteViewController: UIViewController {
         titleLabel.text = selectedToDo.name
     }
     
-    @IBAction func completeTapped(_ sender: Any) {
-//        var index = 0
+    @IBAction func buttonTapped(_ sender: Any) {
+        var index = 0
         for toDo in previousVC.toDos {
             if toDo.name == selectedToDo.name {
-                print("We found it \(toDo.name) \(index)")
+                previousVC.toDos.remove(at: index)
+                previousVC.tableView.reloadData()
+                navigationController?.popViewController(animated: true)
+                break
             }
-//            index += 1
+            index += 1
         }
     }
 }
